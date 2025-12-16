687fe9afbb9052758d14c07fbb737393442b34b2
AntoFruch <Antonin>
2025-12-16 10:43:46 +0100

    project template

---
+++ Assets/MapManager.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class MapManager : MonoBehaviour
+{
+    [SerializeField]
+    private Texture2D image;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        MapGenerator();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void MapGenerator()
+    {
+        Color[] pixels = image.GetPixels();
+        Debug.Log("Total Pixels: " + pixels.Length);
+
+        int width  = image.width;
+        int height = image.height;
+        Debug.Log("Width: " + width + ", Height: " + height);
+
+        Color GRAY   = new Color(229f/255f, 229f/255f, 229f/255f);
+        Color YELLOW = new Color(255f/255f, 233f/255f, 127f/255f);
+        Color RED = new Color(255f/255f, 0f, 0f);
+        Color ORANGE = new Color(255f/255f, 178f/255f, 128f/255f);
+        Color GREEN = new Color(0f, 255f/255f, 33f/255f);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                int index = y * width + x;
+
+                Color pixelColor = pixels[index];
+                switch (pixelColor)
+                {
+                    case var c when c == GRAY:
+                        Instantiate(Resources.Load("prefabs/Edge"), new Vector3(x, 0, y), Quaternion.identity);   
+                        break;
+                    case var c when c == YELLOW:
+                        Instantiate(Resources.Load("prefabs/Path"), new Vector3(x, 0, y), Quaternion.identity);   
+                        break;
+                    case var c when c == RED:
+                        Instantiate(Resources.Load("prefabs/Target"), new Vector3(x, 0, y), Quaternion.identity);   
+                        break;
+                    case var c when c == ORANGE:
+                        Instantiate(Resources.Load("prefabs/Intersection"), new Vector3(x, 0, y), Quaternion.identity);   
+                        break;
+                    case var c when c == GREEN:
+                        Instantiate(Resources.Load("prefabs/Entry"), new Vector3(x, 0, y), Quaternion.identity);   
+                        break;
+                    default :
+                        Instantiate(Resources.Load("prefabs/Tile"), new Vector3(x, 0, y), Quaternion.identity);
+                        break;
+                }
+
+            }
+        }
+    }
+}
