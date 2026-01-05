6adcc08dea0ec144a439effccd021925345d05c9
AntoFruch(Desktop) <antonin.fruchet@telecom-sudparis.eu>
2026-01-05 18:21:19 +0100

    Implement idle rotation for towers and enhance label styling in BuildsWheel

--- Assets/Resources/Buildings/Towers/Tower.cs
+++ Assets/Resources/Buildings/Towers/Tower.cs
@@ -18,6 +18,12 @@ public abstract class Tower : Building
     protected Vector3 lookDirection;
     protected GameObject target = null;
 
+    [SerializeField] float idleRotationChangeTime = 2f;
+    [SerializeField] float idleTurnSpeed = 1f;
+
+    private float idleTimer;
+    private Quaternion idleTargetRotation;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     protected override void Start()
     {
@@ -41,19 +47,39 @@ public abstract class Tower : Building
             clock=0;
         }
     }
+    
+    void UpdateIdleRotation()
+    {
+        idleTimer -= Time.deltaTime;
+
+        if (idleTimer <= 0f)
+        {
+            idleTimer = idleRotationChangeTime;
+
+            float randomY = Random.Range(0f, 360f);
+            idleTargetRotation = Quaternion.Euler(0f, randomY, 0f);
+        }
+
+        rotatingPart.rotation = Quaternion.Lerp(
+            rotatingPart.rotation,
+            idleTargetRotation,
+            idleTurnSpeed * Time.deltaTime
+        );
+    }
     protected void RotateTowardTarget()
     {
         if (target != null)
         {
             lookDirection = (target.transform.position - rotatingPart.position).normalized;
+            Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
+            Vector3 rotation = Quaternion.Lerp(rotatingPart.rotation, lookRotation, lerpStep*Time.deltaTime).eulerAngles;
+            rotatingPart.rotation = Quaternion.Euler(0f, rotation.y, 0f);
             
         } else
         {
-            lookDirection = Vector3.forward;
+            UpdateIdleRotation();
         }
-        Quaternion lookRotation = Quaternion.LookRotation(lookDirection);
-        Vector3 rotation = Quaternion.Lerp(rotatingPart.rotation, lookRotation, lerpStep*Time.deltaTime).eulerAngles;
-        rotatingPart.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+        
     }
     protected abstract void UpdateTarget();
     protected abstract void Shoot();
