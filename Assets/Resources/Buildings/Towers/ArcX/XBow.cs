269b6dec96f70214abeab203d278939e7ee203c3
Juli1 <julien.wa24@gmail.com>
2026-01-09 22:47:36 +0100

    MAJ - New Tower : X-Bow

---
+++ Assets/Resources/Buildings/Towers/ArcX/XBow.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class XBow : Tower
+{
+    [Header("XBow-Specific fields")]
+    [SerializeField] private float shootForce = 1f;
+    [SerializeField] private XBowPrefabs prefabs;
+
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void UpdateTarget()
+    {
+        target = TargetStrategies.Mono(rotatingPart, target, realRange);
+    }
+
+    protected override void Shoot()
+    {
+        GameObject projectile = Instantiate(prefabs.projectile, projectileSpawn.position, Quaternion.LookRotation(lookDirection));
+        projectile.GetComponent<Projectile>().Init(this);
+        
+        projectile.GetComponent<Rigidbody>().AddForce(shootForce * lookDirection, ForceMode.Impulse);
+        Instantiate(prefabs.xbowShotParticles, projectileSpawn.position,Quaternion.LookRotation(lookDirection));
+        
+        projectileSpawn.localPosition = new Vector3(-projectileSpawn.localPosition.x, projectileSpawn.localPosition.y,projectileSpawn.localPosition.z);
+    }
+    
+
+} 
