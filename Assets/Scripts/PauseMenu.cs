da646dded88b9271bcde7b3ecad5f8bcef317661
Juli1 <julien.wa24@gmail.com>
2026-01-14 09:53:00 +0100

    Ajout Fonctionnalité, mais bouton marchent pas
    menu pause en appuyant sur Echap

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,89 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
+
+public class PauseMenu : MonoBehaviour
+{
+    private static bool isPaused = false;
+    InputAction PauseT;
+    [SerializeField]    private PlayerInput playerInput;
+    
+    private VisualElement root; 
+
+    private Button resumeButton;
+
+    private Button quitButton;
+
+    void Start()
+    {
+        PauseT = InputSystem.actions.FindAction("PauseMenu");                //Recherche l'ActionMap "Pause"
+        playerInput.actions.FindActionMap("UI").Enable();                    // Active l'UI
+        playerInput.actions.FindActionMap("Pause").Enable();                 // Active Pause 
+        playerInput.actions.FindActionMap("Player").Enable();                // Active Player 
+        playerInput.actions.FindActionMap("Wheel").Enable();                 // Active Wheel
+        var uiDocument = GetComponent<UIDocument>();                         // Simplification du code 
+        root = uiDocument.rootVisualElement;                                 // Simplification du code
+        root.AddToClassList("hide");                                         // Cache le menu pause au début
+        resumeButton = root.Q<Button>("Resume");                             // Création d'un bouton Resume pour reprendre le jeu
+        quitButton = root.Q<Button>("Quit");                                 // Création d'un bouton Quit pour revenir sur le menu
+
+        resumeButton.RegisterCallback<ClickEvent>(OnResumeButtonClick);      // Callback resume
+        quitButton.RegisterCallback<ClickEvent>(OnQuitButtonClick);          // Callback quit
+    }
+
+
+    private void OnQuitButtonClick(ClickEvent evt)                           // Méthode exécutée quand je clique sur Quit
+    {
+        Debug.Log("Quit button cliqué");
+        SceneManager.LoadScene("MainMenu");
+    }
+
+
+    private void OnResumeButtonClick(ClickEvent evt)                         // Méthode exécutée quand je clique sur Resume
+    {
+        Debug.Log("Resume button cliqué");
+        Resume();
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (PauseT.WasPerformedThisFrame())
+        {
+            Debug.Log("Bouton Appuyé");
+            if (isPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+                
+    }
+    private void Resume()
+    {
+
+        Time.timeScale=1f;
+        isPaused = false;
+        Debug.Log("Le jeu reprend");
+        playerInput.actions.FindActionMap("Player").Enable();
+        playerInput.actions.FindActionMap("Wheel").Enable();
+        root.AddToClassList("hide");
+        
+    }
+    private void Pause()
+    {
+
+        Time.timeScale=0f;
+        isPaused = true;
+        Debug.Log("Le jeu est en pause");
+        root.RemoveFromClassList("hide");
+        playerInput.actions.FindActionMap("Player").Disable();
+        playerInput.actions.FindActionMap("Wheel").Disable();
+        
+    }
+}
