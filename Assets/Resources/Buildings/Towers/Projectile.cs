c19cc4c6a8cbc1299f89d2213cc6257368cd1ef3
AntoFruch(Desktop) <antonin.fruchet@telecom-sudparis.eu>
2026-01-29 00:08:55 +0100

    fix : lifeTime on projectiles

--- Assets/Resources/Buildings/Towers/Projectile.cs
+++ Assets/Resources/Buildings/Towers/Projectile.cs
@@ -4,18 +4,21 @@ public class Projectile : MonoBehaviour
 {
     private Tower tower;
 
+    [SerializeField] float lifeTime = 2;
+    float clock;
     public void Init(Tower tower)
     {
         this.tower = tower;
+        clock = 0;
     }
- 
     // Update is called once per frame
     void Update()
     {
-        if ((transform.position - tower.transform.position).magnitude > tower.CurrentRange)
+        if ((transform.position - tower.transform.position).magnitude > tower.CurrentRange || clock > lifeTime)
         {
             Destroy(gameObject);
         }
+        clock+=Time.deltaTime;
     }
 
     void OnCollisionEnter(Collision collision)
